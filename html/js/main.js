1e1ee7d1227e59fb8d8dcd5bae7b1cfdebd1a669
sandy <sandra.de.la.sancha.aguilar.isc.2000@gmail.com>
2025-04-30 10:29:20 -0600

    cracion archivo js

---
+++ html/js/main.js
@@ -0,0 +1,5 @@
+
+const btnLoadProducts = document.getElementById(btnLoadProducts);
+const URlmain = "https://api.escuelajs.co/api/v1/products";
+
+localStorage.setItem("URlmain",JSON.stringify(URlmain));
